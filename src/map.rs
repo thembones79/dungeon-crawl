7cdbd83de65104f2a0b1e5da98b1f26331025196
thembones79 <michal.szumnarski@gmail.com>
2023-05-27 01:18:24 +0200

    started map rendering

--- src/map.rs
+++ src/map.rs
@@ -6,6 +6,10 @@ pub struct Map {
     pub tiles: Vec<TileType>,
 }
 
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
 impl Map {
     pub fn new() -> Self {
         Self {
