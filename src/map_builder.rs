dd9ef7fa5b6b05d77dd83ef5609e9baf0290fbb0
thembones79 <michal.szumnarski@gmail.com>
2023-06-03 11:49:00 +0200

    started implementing map builder

--- src/map_builder.rs
+++ src/map_builder.rs
@@ -1,1 +1,25 @@
 use crate::prelude::*;
+use crate::TileType;
+const NUM_ROOMS: usize = 20;
+
+pub struct MapBuilder {
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
+}
+impl MapBuilder {
+    fn fill(&mut self, tile: TileType) {
+        self.map.tiles.iter_mut().for_each(|t| *t = tile);
+    }
+
+    fn build_random_rooms(&mut self, rng: &mut RandomNumberGenerator) {
+        while self.rooms.len() < NUM_ROOMS {
+            let room = Rect::with_size(
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_WIDTH - 10),
+                rng.range(1, SCREEN_WIDTH - 10),
+            );
+        }
+    }
+}
