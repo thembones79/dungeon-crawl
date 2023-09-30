986e6f2b58df8ea25bc7f8f9cfa9f70c45cf350a
thembones79 <michal.szumnarski@gmail.com>
2023-09-30 14:15:42 +0200

    :)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ mod prelude {
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
+    pub const DF: &str = "dungeonfont.png";
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
